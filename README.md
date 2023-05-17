03ff17d05b85b120d510cf1687912dfe149a821b
Lucas Siqueira <siqueiraq@gmail.com>
2023-05-17 11:37:35 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # portfolio_dashboard
+
+
+This repository is my streamlit app
+
+To run this project, first install dependencies with
+```
+pip install -r requirements.txt
+```
+
+Run the app with
+```
+streamlit run main.py
+```
+
+You can now view Streamlit app in your browser.
+Local URL: http://localhost:8501
+Network URL: http://192.168.0.7:8501
---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.title("Welcome")
+
+# st.sidebar.title("Pages")
+st.title("Bem vindo ao meu portfolio")
+
+st.markdown("No menu lateral ao lado, você pode acessar alguns dos meu projetos pessoais")
+
+st.subheader("Quem sou eu?")
+st.markdown(
+    """Me chamo Lucas Siqueira Rodrigue, tenho 29 anos e moro em Brasília-DF, etcetcetc
+    """
+)
+
+
